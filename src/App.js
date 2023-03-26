d783bcddc4a106e658d212c1a517cddecdf91a3e
Soulman2131 <sdrame81@gmail.com>
2023-03-26 13:18:45 +0200

    Utilisation des fonctions d'Etat

--- src/App.js
+++ src/App.js
@@ -1,28 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+	const [result, setResult] = useState("");
+
+	const handleClick = e => {
+		setResult(result.concat(e.target.value));
+	};
+
+	const handleClear = () => {
+		setResult("");
+	};
+
+	const handleResult = () => {
+		setResult(eval(result).toString());
+	};
+
 	return (
 		<>
 			<div className="calculatrice">
-				<input type="text" id="main" placeholder="0" />
-				<input type="button" value="0" />
-				<input type="button" value="1" />
-				<input type="button" value="2" />
-				<input type="button" value="3" />
-				<input type="button" value="4" />
-				<input type="button" value="5" />
-				<input type="button" value="6" />
-				<input type="button" value="7" />
-				<input type="button" value="8" />
-				<input type="button" value="9" />
-				<input type="button" value="+" />
-				<input type="button" value="-" />
-				<input type="button" value="*" />
-				<input type="button" value="/" />
-				<input type="button" value="%" />
-				<input type="button" value="." />
-				<input type="button" className="btn" value="clear" />
-				<input type="button" className="btn" value="=" />
+				<h1>Calculatrice</h1>
+				<input type="text" id="main" placeholder="0" value={result} />
+				<input type="button" value="0" onClick={handleClick} />
+				<input type="button" value="1" onClick={handleClick} />
+				<input type="button" value="2" onClick={handleClick} />
+				<input type="button" value="3" onClick={handleClick} />
+				<input type="button" value="4" onClick={handleClick} />
+				<input type="button" value="5" onClick={handleClick} />
+				<input type="button" value="6" onClick={handleClick} />
+				<input type="button" value="7" onClick={handleClick} />
+				<input type="button" value="8" onClick={handleClick} />
+				<input type="button" value="9" onClick={handleClick} />
+				<input type="button" value="." onClick={handleClick} />
+				<input type="button" value="%" onClick={handleClick} />
+				<input type="button" value="+" onClick={handleClick} />
+				<input type="button" value="-" onClick={handleClick} />
+				<input type="button" value="*" onClick={handleClick} />
+				<input type="button" value="/" onClick={handleClick} />
+
+				<input type="button" className="btn" value="Supprimer" onClick={handleClear} />
+				<input type="button" className="btn" value="=" onClick={handleResult} />
 			</div>
 		</>
 	);
