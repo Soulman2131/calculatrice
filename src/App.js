0e3d07e457b307f4021373e14a9ddd6c970ef2ca
Soulman2131 <sdrame81@gmail.com>
2023-03-26 12:52:52 +0200

    Projet calculatrice

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<>
+			<div className="calculatrice">
+				<input type="text" id="main" placeholder="0" />
+				<input type="button" value="0" />
+				<input type="button" value="1" />
+				<input type="button" value="2" />
+				<input type="button" value="3" />
+				<input type="button" value="4" />
+				<input type="button" value="5" />
+				<input type="button" value="6" />
+				<input type="button" value="7" />
+				<input type="button" value="8" />
+				<input type="button" value="9" />
+				<input type="button" value="+" />
+				<input type="button" value="-" />
+				<input type="button" value="*" />
+				<input type="button" value="/" />
+				<input type="button" value="%" />
+				<input type="button" value="." />
+				<input type="button" className="btn" value="clear" />
+				<input type="button" className="btn" value="=" />
+			</div>
+		</>
+	);
 }
 
 export default App;
